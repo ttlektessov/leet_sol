ed076171ca76961f4e1044a0523d4eab2d7c61a1
Temirlan Tlektessov <58460135+ttlektessov@users.noreply.github.com>
2023-01-08 16:27:32 +0900

    Update all_solutions.py

--- all_solutions.py
+++ all_solutions.py
@@ -29,3 +29,26 @@ class Solution:
                 high = mid-1
                 bad = mid
         return bad
+    
+# 98. Validate Binary Search Tree
+# Day 8, 1
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        output =[]
+        self.inorder(root, output)
+        
+        for i in range(1, len(output)):
+            if output[i-1] >= output[i]:
+                return False
+        
+        return True
+    
+    def inorder(self, root, output):
+        if root is None:
+            return
+        
+        self.inorder(root.left, output)
+        output.append(root.val)
+        self.inorder(root.right, output)
+    
