6c16587dc7eacc8410f4e89037aa7ae56c3de0f5
Temirlan Tlektessov <58460135+ttlektessov@users.noreply.github.com>
2023-01-09 22:40:48 +0900

    Update all_solutions.py

--- all_solutions.py
+++ all_solutions.py
@@ -52,3 +52,67 @@ class Solution:
         output.append(root.val)
         self.inorder(root.right, output)
     
+# Day 8, 2
+# 235. Lowest Common Ancestor of a Binary Search Tree
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val < q.val:
+            min_val = p.val
+            max_val = q.val
+        else:
+            min_val = q.val
+            max_val = p.val
+        while True:
+            if min_val <= root.val <= max_val:
+                return root
+            elif max_val < root.val:
+                root = root.left
+            else:
+                root = root.right
+# Day 9, 1
+# 733. Flood Fill
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        cols = len(image[0])
+        rows = len(image)
+        color_to_change = image[sr][sc]
+        
+        def dfs(r, c):
+            nonlocal rows, cols, color, image
+            
+            if r < 0 or c < 0 or r>rows-1 or c>cols-1 or image[r][c]==color or image[r][c]!=color_to_change:
+                return
+            
+            image[r][c] = color
+            
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+ 
+        dfs(sr, sc)
+        return image
+        
+# Day 9, 2
+#
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, i, j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
