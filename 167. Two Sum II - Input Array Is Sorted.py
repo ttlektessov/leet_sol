44ef5dbd1ef74f80400ebdf98534a2bb64450722
Temirlan Tlektessov <58460135+ttlektessov@users.noreply.github.com>
2023-01-25 19:46:20 +0900

    Create 167. Two Sum II - Input Array Is Sorted.py

---
+++ 167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        low = 0
+        high = len(numbers)-1
+        
+        while low < high:
+            if numbers[low] + numbers[high] < target:
+                low = low + 1
+            elif numbers[low] + numbers[high] > target:
+                high = high - 1
+                
+            else:
+                return [low+1, high+1]
